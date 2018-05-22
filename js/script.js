e2abfbcc4fa152022e0d74b42d3f4a4961cd180b
Artem <35399768+atemzaytsev@users.noreply.github.com>
2018-05-22 22:00:46 +0200

    start team section

--- js/script.js
+++ js/script.js
@@ -1,5 +1,24 @@
-//Preloader
+/*
+=========================
+Preloader
+=========================*/
 $(window).on("load", function () {
     $("#status").fadeOut();
     $("#preloader").delay(350).fadeOut("slow");
+});
+/*
+=========================
+Team
+=========================*/
+
+$(function(){
+  $(".owl-carousel").owlCarousel({
+      items:2,
+      autoplay:true,
+      smartSpeed:700,
+      loop:true,
+      autoplayHoverPause:true,
+      nav:true,
+      dots:false  
+  });
 });
