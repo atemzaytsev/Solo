7970a14cdc3a9d76855335fbb69afd4c1fbd3677
Artem <35399768+atemzaytsev@users.noreply.github.com>
2018-05-23 22:36:22 +0200

    animation

--- js/script.js
+++ js/script.js
@@ -10,16 +10,32 @@ $(window).on("load", function () {
 =========================
 Team
 =========================*/
-
-$(function(){
-  $(".owl-carousel").owlCarousel({
-      items:2,
-      autoplay:true,
-      smartSpeed:700,
-      loop:true,
-      autoplayHoverPause:true,
-      nav:true,
-      dots:false,
-      navText:['<i class = "fa fa-angle-left"></i>','<i class = "fa fa-angle-right"></i>' ]
-  });
+$(function () {
+    $(".owl-carousel").owlCarousel({
+        items: 2,
+        autoplay: true,
+        smartSpeed: 700,
+        loop: true,
+        autoplayHoverPause: true,
+        nav: true,
+        dots: false,
+        navText: ['<i class = "fa fa-angle-left"></i>', '<i class = "fa fa-angle-right"></i>']
+    });
+});
+/*
+=========================
+Progress bars
+=========================*/
+$(function () {
+    $("#progress-elements").waypoint(function () {
+        $(".progress-bar").each(function () {
+            $(this).animate({
+                width: $(this).attr("aria-valuenow") + "%"
+            }, 1000);
+        });;
+        
+        this.destroy();
+    }, {
+        offset: "bottom-in-view"
+    });
 });
